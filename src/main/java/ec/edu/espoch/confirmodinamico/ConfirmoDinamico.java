4caf1a3c21ae52a886e3feb8c70e8bf64b8463dc
WilliAn-333 <www.pegazo23@gmail.com>
2025-01-10 15:43:38 -0500

    Polimorfismo Dinamico

---
+++ src/main/java/ec/edu/espoch/confirmodinamico/ConfirmoDinamico.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.confirmodinamico;
+
+import ec.edu.espoch.confirmodinamico.clases.Estudiante;
+
+
+
+/**
+ *
+ * @author USER
+ */
+public class ConfirmoDinamico {
+
+    public static void main(String[] args) {
+    
+        
+        Estudiante objEstudiante=new Estudiante(5258,"62595","Willian","Caisaguano");
+    }
+}
